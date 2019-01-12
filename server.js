1a06d72e0bfb607f601d87a2f3e09ca0bd14981c
Sudarshan Shetty <sudarshanshetty6798@gmail.com>
2019-01-12 12:39:39 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var  app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = `${now},${req.method} ${req.url}`
+  console.log(log);
+
+  fs.appendFileSync('server.log',log+'\n',(err)=>{
+    if(error){
+      console.log('Unable to connect to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req,res,next)=>{
+//   res.render('maintenance.hbs')
+// });
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getcurrentYear',()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+})
+
+app.get('/',(req,res) =>{
+  res.render('home.hbs',{
+    pageTitle:'Home Page',
+    welcome :'Welcome to my Website'
+  })
+});
+
+app.get('/about',(req,res) =>{
+  res.render('about.hbs',{
+    pageTitle:'About Page'
+  });
+});
+
+app.get('/error',(req,res)=>{
+  res.send({
+    errorMessage :'Unable to fulfill request'
+  });
+})
+app.listen(3000,()=>{
+  console.log('Server is up on Port 3000');
+});
